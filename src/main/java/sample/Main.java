6a5af1530118553c61e5e779a28b2dc6345a28cd
Justyna Koźlik <jkozlik96@gmail.com>
2021-02-02 18:42:00 +0100

    + Create main view of the application

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main-view.fxml"));
+        primaryStage.setTitle("Count&Convert");
+        primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
 
