883c60e0976d2f268100fc45b4953ee0c53a8738
Justyna Koźlik <jkozlik96@gmail.com>
2021-02-04 14:47:37 +0100

    + Create convert app view

--- src/main/java/sample/controller/ConvertViewController.java
+++ src/main/java/sample/controller/ConvertViewController.java
@@ -1,4 +1,11 @@
 package sample.controller;
 
+import javafx.scene.control.Button;
+
 public class ConvertViewController {
+
+    public Button cToFButton;
+    public Button fToCButton;
+    public Button cToKButton;
+    public Button kToCButton;
 }
