5136cb11a1377cb406201fb42ce06dc0cba7863f
Justyna Koźlik <jkozlik96@gmail.com>
2021-02-04 12:41:05 +0100

    + Create a controller for the main view of the application (initialization of convert and count button)

--- src/main/java/sample/controller/MainViewController.java
+++ src/main/java/sample/controller/MainViewController.java
@@ -1,4 +1,59 @@
 package sample.controller;
 
-public class MainViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private BorderPane mainViewBorderPane;
+    @FXML
+    private Button countButton;
+    @FXML
+    private Button convertButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeCountButton();
+        initializeConvertButton();
+    }
+
+    private void initializeConvertButton() {
+        convertButton.setOnAction((x) -> {
+            try {
+                Stage stage = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("/fxml/convert-view.fxml"));
+                stage.setTitle("Convert");
+                stage.setScene(new Scene(root, 700, 500));
+                stage.show();
+            } catch (IOException e) {
+               throw new RuntimeException("Can't load this file");
+            }
+        });
+    }
+
+    private void initializeCountButton() {
+        countButton.setOnAction((x) -> {
+            try {
+                Stage stage = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("/fxml/count-view.fxml"));
+                stage.setTitle("Count");
+                stage.setScene(new Scene(root, 700, 500));
+                stage.show();
+            } catch (IOException e) {
+                throw new RuntimeException("Can't load this file");
+            }
+
+        });
+    }
 }
