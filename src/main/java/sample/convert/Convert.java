457cb37861c0596f037a298304bb56db627f7eb4
Justyna Koźlik <jkozlik96@gmail.com>
2021-02-06 11:59:09 +0100

    * Implementation of all convert methods in Convert class

--- src/main/java/sample/convert/Convert.java
+++ src/main/java/sample/convert/Convert.java
@@ -15,4 +15,28 @@ public class Convert {
         return solution;
     }
 
+    public double cToFConvert(double c){
+        double solution = c * 1.8000 + 32.00;
+        return  solution;
+    }
+
+    public double fToKConvert(double f){
+        double solution = (f - 32.00)/1.8000 + 273.15;
+        return solution;
+    }
+
+    public double fToCConvert(double f){
+        double solution = (f - 32.00)/1.8000;
+        return solution;
+    }
+
+    public double kToCConvert(double k){
+        double solution = k - 273.15;
+        return solution;
+    }
+
+    public double kToFConvert(double k){
+        double solution = (k - 273.15) * 1.8000 + 32.00;
+        return solution;
+    }
 }
