211f8936528709937379eb3bd2ea55555eac59ef
Justyna Koźlik <jkozlik96@gmail.com>
2021-02-18 11:39:53 +0100

    + Create count app view

--- src/main/java/sample/controller/CountViewController.java
+++ src/main/java/sample/controller/CountViewController.java
@@ -1,4 +1,10 @@
 package sample.controller;
 
+import javafx.scene.control.Button;
+
 public class CountViewController {
+    public Button areaButton;
+    public Button perimeterButton;
+    public Button volumeButton;
+    public Button quadraticEquationButton;
 }
