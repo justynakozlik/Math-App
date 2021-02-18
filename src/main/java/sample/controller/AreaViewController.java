dcacfde005c094fbd0b1071e763aa9bf633ab4cb
Justyna Koźlik <jkozlik96@gmail.com>
2021-02-18 13:27:30 +0100

    + Create view for area counting

--- src/main/java/sample/controller/AreaViewController.java
+++ src/main/java/sample/controller/AreaViewController.java
@@ -1,4 +1,19 @@
 package sample.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class AreaViewController {
+    public ChoiceBox areaChoiceBox;
+    public Label firstParamLabel;
+    public Label secondParamLabel;
+    public Label thirdParamLabel;
+    public Button countButton;
+    public Button refreshButton;
+    public TextField firstParamTextField;
+    public TextField secondParamTextField;
+    public TextField thirdParamTextField;
+    public Label solutionLabel;
 }
