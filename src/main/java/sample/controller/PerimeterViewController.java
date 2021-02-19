158e051318d5ef6d02a1db0910758cdaaf1a2f12
Justyna Koźlik <jkozlik96@gmail.com>
2021-02-19 14:44:05 +0100

    + Create view for perimeter counting

--- src/main/java/sample/controller/PerimeterViewController.java
+++ src/main/java/sample/controller/PerimeterViewController.java
@@ -1,4 +1,22 @@
 package sample.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class PerimeterViewController {
+
+    public ChoiceBox perimeterChoiceBox;
+    public Label firstParamLabel;
+    public Label secondParamLabel;
+    public Label thirdParamLabel;
+    public Label fourthParamLabel;
+    public Button countButton;
+    public Button refreshButton;
+    public TextField firstParamTextField;
+    public TextField secondParamTextField;
+    public TextField thirdParamTextField;
+    public TextField fourthParamTextField;
+    public Label solutionLabel;
 }
