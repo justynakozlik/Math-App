a5968a175baf8565a021b766c7dd9f586ba638ac
Justyna Koźlik <jkozlik96@gmail.com>
2021-02-22 12:21:15 +0100

    + Create view for quadratic equation counting

--- src/main/java/sample/controller/QuadraticEquationViewController.java
+++ src/main/java/sample/controller/QuadraticEquationViewController.java
@@ -1,4 +1,14 @@
 package sample.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class QuadraticEquationViewController {
+    public TextField firstParamTextField;
+    public TextField secondParamTextField;
+    public TextField thirdParamTextField;
+    public Button countButton;
+    public Button refreshButton;
+    public Label solutionLabel;
 }
